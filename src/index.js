e60a952a80bea641f5d285a00ecc4bb9b1269e82
Junaid Ahmed <junaid@simpleux.co.uk>
2020-03-15 07:21:29 +0000

    feat(select-image): Select Image | 002 - Allow user to select an image

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 /* istanbul ignore file */
 
+import * as tf from '@tensorflow/tfjs'
+import { setWasmPath } from '@tensorflow/tfjs-backend-wasm'
+
+import BackgroundRemoval from './component/background-removal/background-removal'
 import AlertService from './component/alert/alert'
 
 import './component/core-css'
+import './component/input-source'
 
 async function main () {
   AlertService.init()
+
+  tf.enableProdMode()
+  setWasmPath('/assets/tfjs-backend-wasm.wasm')
+  await tf.setBackend('wasm')
+
+  AlertService.announce('Loading Necessary image processing files.')
+  const bgRemovalInstance = new BackgroundRemoval()
+  await bgRemovalInstance.loadModel()
+
   AlertService.announce('Application is ready to use')
 }
 
