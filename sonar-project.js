52d6f95f82ac074d0019128219739aa0f4f331a3
Junaid Ahmed <me@junaid.eu>
2021-07-03 11:50:25 +0100

    chore(deps): updated project dependencies (#5)

--- sonar-project.js
+++
@@ -1,9 +0,0 @@
-const sonarqubeScanner = require('sonarqube-scanner');
-
-sonarqubeScanner({
-    serverUrl: 'http://localhost:9000',
-    options : {
-        'sonar.sources': '.',
-        'sonar.inclusions' : 'src/**'
-    }
-}, () => {});
